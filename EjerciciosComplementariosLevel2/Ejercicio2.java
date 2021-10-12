2d4b52c00078caaa927a5a656f58afbfa9632fd1
sotelojavier22 <sotelojavier22@gmail.com>
2021-10-12 09:44:01 -0300

    Ejercicio 2

---
+++ EjerciciosComplementariosLevel2/Ejercicio2.java
@@ -0,0 +1,35 @@
+package EjerciciosComplementariosLevel2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        List<Integer> lista = new ArrayList<>();
+        // cargo lista
+        cargaLista(lista);
+        // imprimo lista
+        System.out.println("Lista original\n");
+        imprimirLista(lista);
+        // agrego un elemento al principio y al final de la lista
+        lista.add(0, 23);
+        lista.add(6, 32);
+        // imprimo lista
+        System.out.println("Lista modificada");
+        imprimirLista(lista);
+    }
+
+    public static void cargaLista(List<Integer> lista) {
+        lista.add(11);
+        lista.add(80);
+        lista.add(19);
+        lista.add(22);
+        lista.add(40);
+    }
+
+    public static void imprimirLista(List<Integer> lista) {
+        for (Integer elemento : lista) {
+            System.out.println(elemento);
+        }
+    }
+}
